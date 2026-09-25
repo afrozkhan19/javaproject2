8f7b024011e60d8823a0019ee4bed8a261d2fcab
afrozkhan19 <161969856+afrozkhan19@users.noreply.github.com>
2026-09-25 11:06:40 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -4,7 +4,7 @@ public class Demo
 public static void main(String args[])
 {
 
-  System.out.println("this is my first java programe");
+  System.out.println("this is my latest java programe");
 
 }
 
